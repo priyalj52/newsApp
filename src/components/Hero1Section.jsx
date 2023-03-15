444ef0e694a203221d4c4211f582a7cbdcbb2c06
priyal <priyaljaiin52@gmail.com>
2023-03-15 18:32:15 +0530

    added hero ,recommendation page

--- src/components/Hero1Section.jsx
+++ src/components/Hero1Section.jsx
@@ -4,112 +4,65 @@ import Food from "../images/food.jpg"
 import Business from "../images/business.jpg"
 import Sports from "../images/sports.jpg"
 const Hero1Section = () => {
-      const [subject, setsubject] = useState("");
-      // useEffect(() => {
-      //       const fetchapi = async () => {
-      //             const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=e4f4d0a0b670491eb0e397ff14666409`);
-      //             console.log(response);
-      //       }
-      //       fetchapi();
-      // }, []);
-      return (
-            <div>
-                  <div className='flex flex-wrap w-full gap-3 p-4 justify-between font-bold text-sm'>
-                        <div className='cursor-pointer hover:text-red-600'>All News</div>
-                        <div className='cursor-pointer hover:text-red-600'>Politics</div>
-                        <div className='cursor-pointer hover:text-red-600'>Sports</div>
-                        <div className='cursor-pointer hover:text-red-600'>Entertaiment</div>
-                        <div className='cursor-pointer hover:text-red-600'>General</div>
-                        <div className='cursor-pointer hover:text-red-600'>International</div>
-                        <div className='cursor-pointer hover:text-red-600'>World</div>
-                        <div className='cursor-pointer hover:text-red-600'>India</div>
+  const [herodata, setherodata] = useState([]);
+  useEffect(() => {
+    const fetchapi = async () => {
+      if (localStorage.getItem("Heronews") === undefined || localStorage.getItem("Heronews") === null) {
+        await fetch(`https://newsapi.org/v2/top-headlines/sources?category=business&pageSize=6&apiKey=e4f4d0a0b670491eb0e397ff14666409`)
+          .then((res) => res.json())
+          .catch((Err) => console.log(Err))
+          .then(res => { setherodata(res.sources); localStorage.setItem("Heronews", JSON.stringify(res.sources)) });
+      } else {
+        setherodata(JSON.parse(localStorage.getItem("Heronews")));
+      }
+    }
+    fetchapi();
+  }, []);
+  return (
+    <div>
+      <div className='flex flex-wrap w-full gap-3 p-4 justify-between font-bold text-sm'>
+        <div className='cursor-pointer hover:text-red-600'>All News</div>
+        <div className='cursor-pointer hover:text-red-600'>Politics</div>
+        <div className='cursor-pointer hover:text-red-600'>Sports</div>
+        <div className='cursor-pointer hover:text-red-600'>Entertaiment</div>
+        <div className='cursor-pointer hover:text-red-600'>General</div>
+        <div className='cursor-pointer hover:text-red-600'>International</div>
+        <div className='cursor-pointer hover:text-red-600'>World</div>
+        <div className='cursor-pointer hover:text-red-600'>India</div>
+      </div>
+      <div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner bg-slate-400 w-full md:p-5 border-[1px] border-black">
+            {
+              herodata && herodata.length !== 0 && herodata.map((val , idx) =>(
+                <div className={`${idx === 0 ? 'carousel-item active' : 'carousel-item'}`}>
+                  <div className='w-full border-[1px] border-black'>
+                    <img class="w-1/2" src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Nature-wallpapers-Full-HD-backgroud.jpg" alt="First slide" />
                   </div>
-                  <div>
-                  {/* <div class="max-w-2xl m-auto">
-  <div id="default-carousel" class="relative mb-4 mt-4 ml-4" data-carousel="static">
+                </div>
+              ))
+            }
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
 
-    <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
 
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <span class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-            <img src={Politics} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
-        </div>
-        
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Food} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
-        </div>
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src={Business} class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
-        </div>
+
+      </div>
     </div>
-    <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-    </div>
-    <button type="button" class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span class="hidden">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-            <span class="hidden">Next</span>
-        </span>
-    </button>
-</div>
-  <p class="ml-4">This carousel component is part of the <a class="text-blue-600 hover:underline" href="https://flowbite.com/docs/components/carousel/" target="_blank">Flowbite component library.</a></p>
-</div> */}
-
-
-
-
-
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner bg-slate-400 w-full md:p-5 border-[1px] border-black">
-    <div class="carousel-item active">
-        <div className='w-full border-[1px] border-black'>
-        <img class="w-1/2" src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Nature-wallpapers-Full-HD-backgroud.jpg" alt="First slide"/>
-
-        </div>
-    </div>
-    <div class="carousel-item">
-    <div className='w-full border-[1px] border-black'>
-        <img class="w-1/2" src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Nature-wallpapers-Full-HD-backgroud.jpg" alt="First slide"/>
-
-        </div>
-    </div>
-    <div class="carousel-item">
-    <div className='w-full border-[1px] border-black'>
-        <img class="w-1/2" src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Nature-wallpapers-Full-HD-backgroud.jpg" alt="First slide"/>
-
-        </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-
-
-
-
-
-                  </div>
-            </div>
-      )
+  )
 }
 
 export default Hero1Section
