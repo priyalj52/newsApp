7a05554dcb2c7116de3f3ffe8f642f0e33499b31
priyal <priyaljaiin52@gmail.com>
2023-03-15 18:31:40 +0530

    added recommendation page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import  Navbar from "./components/Navbar";
 import Prediction from './components/Prediction';
 import Hero1Section from './components/Hero1Section';
+import Recommendation from './components/Recommendation';
 function App() {
   return (
     <div className='w-full h-screen bg-slate-100'>
      <Navbar/>
      <Hero1Section/>
      <Prediction/>
+     <Recommendation/>
     </div>
   );
 }
