ec94bf9cde5aca173b1bb2d711781b55216c3fd6
priyal <priyaljaiin52@gmail.com>
2023-03-16 20:37:24 +0530

    added navigation

--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 // import { IoFastFoodSharp } from "react-icons/io5"
 import Politics from "../images/politics.jpg"
 import Food from "../images/food.jpg"
@@ -7,22 +7,47 @@ import Sports from "../images/sports.jpg"
 import Entertainment from "../images/entertainment.jpg"
 import Culture from "../images/culture.jpg"
 import Science from "../images/science.jpg"
+import { useNavigate } from "react-router-dom";
 const Prediction = () => {
+    const navigate=useNavigate();
     return (
         <div className="predict">
             <div className="p-header">
                 <h1 className="font-bold text-2xl mb-9" >   Popular Recommendations</h1>
-                <div className="icons flex w-full flex-wrap justify-between">
+                <div className="icons flex w-full flex-wrap justify-between cursor-pointer">
                  
                     
-                <img src={Politics} alt="politics" className="w-40 pr-2  rounded-full ml-4"   />
+                <img onClick={()=>{
+                    localStorage.setItem("subject" , "politics");
+                    navigate('/CustomNews');
+                }} src={Politics} alt="politics" className="w-40 pr-2  rounded-full ml-4"   />
               
-                <img src={Food} alt="health" className="w-40   rounded-full " />
-                <img src={Sports} alt="sports" className="w-40   rounded-full " />
-                <img src={Entertainment} alt="entertainment" className="w-40   rounded-full " />
-                <img src={Culture} alt="general" className="w-40  rounded-full " />
-                <img src={Business} alt="business" className="w-40  rounded-full " />
-                <img src={Science} alt="science" className="w-40  rounded-full " />
+                <img 
+                onClick={()=>{
+                    localStorage.setItem("subject","Food")
+              navigate('/CustomNews')    
+              
+                }}
+                
+                src={Food} alt="health" className="w-40   rounded-full " />
+
+                <img    onClick={()=>{
+                    localStorage.setItem("subject","Sports")
+              navigate('/CustomNews')  }}
+                src={Sports} alt="sports" className="w-40   rounded-full " />
+                
+                <img  onClick={()=>{
+                    localStorage.setItem("subject","Entertainment")
+              navigate('/CustomNews')  }} src={Entertainment} alt="entertainment" className="w-40   rounded-full " />
+                <img  onClick={()=>{
+                    localStorage.setItem("subject","Culture")
+              navigate('/CustomNews')  }} src={Culture} alt="general" className="w-40  rounded-full " />
+                <img   onClick={()=>{
+                    localStorage.setItem("subject","Business")
+              navigate('/CustomNews')  }}src={Business} alt="business" className="w-40  rounded-full " />
+                <img  onClick={()=>{
+                    localStorage.setItem("subject","Science")
+              navigate('/CustomNews')  }} src={Science} alt="science" className="w-40  rounded-full " />
                 </div>
             </div>
         </div>
--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react'
 const Recommendation = () => {
   const [data, setData] = useState([]);
   
- 
+  const filter_data = (str)=>{
+    return str.split("T")[0];
+  }
   useEffect(() => {
     const handleError = (err) => {
       console.log(`Failed ${err}`);
     }
     const fetchapi = async () => {
-
       if(localStorage.getItem("TopHeadnews") === undefined || localStorage.getItem("TopHeadnews") === null)
       {
       const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&pageSize=3&apiKey=64fca12e4bed424e9c1b9118b8a2da9f`);
@@ -26,23 +27,23 @@ const Recommendation = () => {
   }, [])
 
   return (
-    <div>
-      <h1 className='font-bold text-base mb-6'>Recommendation Topic </h1>
+    <div >
+      <h1 className='font-bold text-base mb-6 ml-12'>Recommendation Topic </h1>
       <div>
         {
         data && data.length !== 0 &&  data.map((val , idx) => {
          
           return (
           <div key={idx} className="heading" >
-            <div className="top flex">
+            <div className="top flex justify-around w-100">
             <a href={val.url}className="font-medium text-sm">{val.title}</a>
             <img src={val.urlToImage} alt="img" className='w-40 ml-4' />
             </div>
-            <div className="bottom flex ">
-            <div className="author text-orange mr-4">{val.author}
+            <div className="bottom flex ml-24 ">
+            <div className="author text-orange mr-4">{val.source.name}
             </div>
             
-            <div className="publish">date</div>
+            <div className="publish mb-10 text-blue">{filter_data(val.publishedAt)}</div>
           </div>
             </div>
             
